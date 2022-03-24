a057261ae3743817af5dc899964915957fd18b44
Arbigate <100032895+Arbigate@users.noreply.github.com>
2022-03-24 11:55:48 -0400

    Update modLinkExceptions.py

--- modLinkExceptions.py
+++ modLinkExceptions.py
@@ -1,9 +1,14 @@
 manualExceptions = {
-'elfx':'https://www.nexusmods.com/skyrimspecialedition/mods/2424',
 'skse':'https://skse.silverlock.org/',   
 'enbseries':'http://enbdev.com/download_mod_tesskyrimse.htm',
-'nff':'https://www.nexusmods.com/skyrimspecialedition/mods/55653',
 'lux':'https://www.nexusmods.com/skyrimspecialedition/mods/43158',
-'ussep':'https://www.nexusmods.com/skyrimspecialedition/mods/266',
-'mo2':'https://www.nexusmods.com/skyrimspecialedition/mods/6194'
+'enhanced blood textures':'https://www.nexusmods.com/skyrimspecialedition/mods/2357'
+}
+
+manualNexusExceptions = {
+'elfx':'Enhanced Lights and FX',
+'nff':'Nether Follower Framework',
+'ussep':'Unofficial Skyrim Special Edition Patch',
+'mo2':'Mod Organizer 2',
+'lotd':'Legacy of the Dragonborn'
 }
