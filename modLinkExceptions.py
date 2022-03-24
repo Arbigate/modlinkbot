ea110c5e1a29ed28bda72e7fc4b953f5bd9cae9a
Arbigate <100032895+Arbigate@users.noreply.github.com>
2022-03-23 21:43:23 -0400

    Create modLinkExceptions.py

---
+++ modLinkExceptions.py
@@ -0,0 +1,9 @@
+manualExceptions = {
+'elfx':'https://www.nexusmods.com/skyrimspecialedition/mods/2424',
+'skse':'https://skse.silverlock.org/',   
+'enbseries':'http://enbdev.com/download_mod_tesskyrimse.htm',
+'nff':'https://www.nexusmods.com/skyrimspecialedition/mods/55653',
+'lux':'https://www.nexusmods.com/skyrimspecialedition/mods/43158',
+'ussep':'https://www.nexusmods.com/skyrimspecialedition/mods/266',
+'mo2':'https://www.nexusmods.com/skyrimspecialedition/mods/6194'
+}
