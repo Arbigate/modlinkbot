86a10c78caf5059a4284c8287444dc12c2260bd3
Arbigate <100032895+Arbigate@users.noreply.github.com>
2022-05-21 14:38:00 -0400

    Add files via upload

---
+++ cogs/error_reporting.py
@@ -0,0 +1,55 @@
+
+"""
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+import discord
+from discord.ext import commands
+
+
+class ErrorReporting(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+
+    @commands.command(name='error')
+    async def error(self, ctx):
+        user = self.bot.get_user(668828647653638174)
+        if user is None:
+            user = await self.bot.fetch_user(668828647653638174)
+            sender_id = ctx.message.author.id
+            sender_name = ctx.message.author.name
+            channel = ctx.message.channel.name
+        embed = discord.Embed(title="Modlink Error Reported")
+        embed.add_field(name="Reporter:", value=f"Name: {sender_name} \n ID: {sender_id}", inline=False)
+        embed.add_field(name="Channel:", value=f"#{channel}", inline=False)
+        await user.send(embed=embed)
+        await ctx.send("The error has been reported. Thank you!")
+
+
+
+
+    #@commands.command(name='error')
+    #async def error(self, ctx):
+    #    user = self.bot.get_user(668828647653638174)
+    #    if user is None:
+    #        user = await self.bot.fetch_user(668828647653638174)
+    #        sender = ctx.message.author.id
+    #        channel = ctx.message.channel.name
+        
+    #    await user.send(f"There is an issue with Modlink. Notified by {sender} in #{channel}.")
+
+
+
+def setup(bot):
+    bot.add_cog(ErrorReporting(bot))
